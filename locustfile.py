2138c4413c8d7c2ad2b8761d8ff5c6e84cb2a225
kalicg27 <102329198+kalicg27@users.noreply.github.com>
2025-11-27 16:31:07 +0100

    change_01

--- locustfile.py
+++ locustfile.py
@@ -1,37 +1,5 @@
-from locust import events
-from tests.utils import TARGET_HOST, LOAD_PROFILE
 from tests.user_scenarios import ReadOnlyUser, WriteHeavyUser
+from tests.utils import TARGET_HOST
 
-
-# Expose host on users
 ReadOnlyUser.host = TARGET_HOST
 WriteHeavyUser.host = TARGET_HOST
-
-
-@events.init_command_line_parser.add_listener
-def _(parser):
-    """
-    Extend Locust CLI with sensible defaults from YAML if not provided.
-    This makes it easy to run locally and in CI.
-    """
-    parser.add_argument(
-        "--users",
-        type=int,
-        env_var="LOCUST_USERS",
-        default=LOAD_PROFILE["users"],
-        help="Number of concurrent users (default from config/settings.yaml)."
-    )
-    parser.add_argument(
-        "--spawn-rate",
-        type=float,
-        env_var="LOCUST_SPAWN_RATE",
-        default=LOAD_PROFILE["spawn_rate"],
-        help="Spawn rate users/sec (default from config/settings.yaml)."
-    )
-    parser.add_argument(
-        "--run-time",
-        type=str,
-        env_var="LOCUST_RUN_TIME",
-        default=LOAD_PROFILE["run_time"],
-        help="Run time, e.g. 1m, 5m (default from config/settings.yaml)."
-    )
